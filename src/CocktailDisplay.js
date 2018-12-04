df6cf9e6e54a611a8e14cd7f7d18497a27a5db25
milanla <milan.lanlada@gmail.com>
2018-12-03 23:13:50 -0500

    added dynamic form

--- src/CocktailDisplay.js
+++ src/CocktailDisplay.js
@@ -3,18 +3,23 @@ import React, { Component } from 'react';
 const CocktailDisplay = ({match, cocktail, cocktailDetail}) => {
 
   let arrayOfProportions = cocktailDetail.map(p => {
-    return <div>{p.ingredient_name}</div>
+    return (
+      <div>
+        <div className="left">{p.ingredient_name}</div>
+        <div className="right">{p.amount}</div>
+      </div>
+    )
   })
 
   return (
     cocktail.id === parseInt(match.params.cocktailId) ? (<div className="column">
-      <div className="ui segment">
+      <div className="ui segment" id="display">
       <h1>{cocktail.name}</h1>
       <p>{cocktail.description}</p>
       <h3>Instructions</h3>
       <p>{cocktail.instructions}</p>
       <h3>Ingredients</h3>
-      <div>{arrayOfProportions}</div>
+      {arrayOfProportions}
       </div>
     </div>) : null
   )
--- src/Form.js
+++ src/Form.js
@@ -12,16 +12,9 @@ export default class Form extends Component {
   }
 
   handleChange = (e) => {
-    // console.log(e.target.value)
-    if (["ingredient_name", "amount"].includes(e.target.name) ) {
-      let proportions = [...this.state.proportions]
-
-      proportions[0][e.target.name] = e.target.value.toUpperCase()
-
-      this.setState({ proportions }, () => console.log(this.state))
-    } else {
-      this.setState({ [e.target.name]: e.target.value.toUpperCase() })
-    }
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   submitHandler = (e, obj) => {
@@ -29,12 +22,58 @@ export default class Form extends Component {
     console.log(obj)
   }
 
+  handleClick = (e) => {
+    this.setState({
+      proportions: [...this.state.proportions, { ingredient_name: '', amount: ''}]
+    })
+  }
+
+  handleIngredientChange = (e, idx) => {
+    const newIng = this.state.proportions.map((obj, objIdx) => {
+      if (idx !== objIdx) {
+        return obj
+      }
+      return {...this.state.proportions, [e.target.name]: e.target.value}
+    })
+    this.setState({ proportions: newIng })
+  }
+
+  addIngredient = () => {
+    const { proportions } = this.state
+
+    return proportions.map((obj, idx) => {
+      return (
+        <div key={idx} className="ui form">
+          <div className="field">
+            <input
+              type="text"
+              value={obj.ingredient_name}
+              name="ingredient_name"
+              placeholder="Ingredient Name"
+              onChange={e => this.handleIngredientChange(e, idx)}
+            />
+          </div>
+          <div className="field">
+            <input
+              type="text"
+              value={obj.amount}
+              name="amount"
+              placeholder="Amount"
+              onChange={e => this.handleIngredientChange(e, idx)}
+            />
+          </div>
+          <br />
+        </div>
+      )
+    })
+  }
+
   render() {
-    const {name, description, instructions, proportions} = this.state
+    const { name, description, instructions } = this.state
 
     return (
       <div className="column">
-      <div className="ui segment">
+      <div className="ui segment" id="form">
         <h2>Add a New Cocktail</h2>
         <form className="ui form" onSubmit={(e) => this.submitHandler(e, this.state)}>
           <div className="field">
@@ -48,7 +87,13 @@ export default class Form extends Component {
           <input type="text" name="instructions" placeholder="Instructions" value={instructions}
           onChange={(e)=> this.handleChange(e)}/>
           </div>
-          <input className="ui small green button" type="submit" value="Add Ingredient"/>
+          <div className="field">
+          <input className="ui small green button" type="submit" value="Add Ingredient" onClick={e => this.handleClick(e)}/>
+          </div>
+          {this.addIngredient()}
+          <div className="field">
+            <input className="ui small green button" type="submit" value="Create New Cocktail"/>
+          </div>
         </form>
         </div>
       </div>
